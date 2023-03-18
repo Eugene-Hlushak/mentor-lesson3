c59a95a08e34847c858bbd10d19deff4a554fd9d
Artem Riabchych <artemskyart@gmail.com>
2023-03-18 13:02:01 +0200

    alldone

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,14 +1,24 @@
+import { useState } from 'react';
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
 
-export const SearchForm = () => {
+export const SearchForm = ({ hadleSubmit }) => {
+  const [query, setQuery] = useState("")
+  const handleImput = (evt) => {
+  setQuery(evt.target.value)
+}
+  const hendleFormSubmit = (evt) => {
+    evt.preventDefault();
+    hadleSubmit(query);
+    setQuery("")
+}
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={hendleFormSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select aria-label="select" onChange={handleImput} name="region" required>
+        <option  disabled defaultValue="">
           Select a region and press enter
         </option>
         <option value="africa">Africa</option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,44 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useState, useEffect } from 'react';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  
+  const [region, setRegion] = useState('');
+  const [countries, setCountries] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoad, setIsLoad] = useState(false);
+
+  const hadleSubmit = query => {
+    setRegion(query);
+    setError(null);
+  };
+
+  useEffect(() => {
+    setIsLoad(true);
+    const Countries = async () => {
+      try {
+        const result = await fetchByRegion(region);
+        setCountries(result);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setIsLoad(false);
+      }
+    };
+
+    Countries();
+  }, [region]);
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        <SearchForm hadleSubmit={hadleSubmit} />
+        {isLoad && <Loader />}
+        {countries && <CountryList countries={countries} />}
+        {error && <Heading>Sorry, something goes wrong</Heading>}
       </Container>
     </Section>
   );
